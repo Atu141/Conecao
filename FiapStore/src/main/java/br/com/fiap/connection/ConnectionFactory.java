d7a39041453216c24b1ced6d81523c6dd613fae2
Arthur Badu Reis <143804510+Atu141@users.noreply.github.com>
2024-10-15 08:41:55 -0300

    Add files via upload

--- FiapStore/src/main/java/br/com/fiap/connection/ConnectionFactory.java
+++ FiapStore/src/main/java/br/com/fiap/connection/ConnectionFactory.java
@@ -22,8 +22,8 @@ public class ConnectionFactory {
 		Connection connection = null;
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			connection = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "rm98845",
-					"290604");
+			connection = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "rm99831",
+					"080705");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
